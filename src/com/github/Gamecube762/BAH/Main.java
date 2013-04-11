50bbb4695213141dd3de50d221306d3b02ffcc59
Kyle Mollart <gamecube762@hotmail.com>
2013-04-11 16:47:47 -0700

    Added permissions Support

--- src/com/github/Gamecube762/BAH/Main.java
+++ src/com/github/Gamecube762/BAH/Main.java
@@ -24,36 +24,61 @@ public class Main extends JavaPlugin{
 	public boolean onCommand(CommandSender sender, Command cmd, String labal, String[] args) {
 		int PlayersOnline = Bukkit.getServer().getOnlinePlayers().length;
 		Player s = Bukkit.getServer().getPlayer(sender.getName());
+		
+		//boolean sIsPlayer = true; //WIP
 		Location sLoc = s.getLocation();
+			
+			
 		
 		if(cmd.getName().equalsIgnoreCase("bringallhere")|cmd.getName().equalsIgnoreCase("bah")){
-			if (PlayersOnline <= 1){s.sendMessage(ChatColor.RED + "No one is online to teleport to you ='(");return true;}
-			for(Player p: Bukkit.getServer().getOnlinePlayers()){
-				if (p!=s) {
-					p.teleport(sLoc);
-					p.sendMessage(ChatColor.GOLD + "Teleported all to " + s.getName());
+			if (s.hasPermission("bah.command.bringallto")|s.hasPermission("bah.command.*")) {
+				
+				if (PlayersOnline <= 1){s.sendMessage(ChatColor.RED + "No one is online to teleport to you ='(");return true;}
+				
+				for(Player p: Bukkit.getServer().getOnlinePlayers()){
+					if (!(p.hasPermission("bah.teleport.exempt")))
+						p.teleport(sLoc);
+						p.sendMessage(ChatColor.GOLD + "Teleported all to " + s.getName());
 				}
-			}	
-			s.sendMessage(ChatColor.GOLD + "Teleported all to you!");
-			getLogger().info(s.getName() + " teleported all to him/her");
-			return true;
+				
+				s.sendMessage(ChatColor.GOLD + "Teleported all to you!");
+				getLogger().info(s.getName() + " teleported all to him/her");
+				return true;
+			
+			} else {
+				s.sendMessage(ChatColor.RED + "You don't have permission!");
+				return true;
+		}
 		} else if(cmd.getName().equalsIgnoreCase("bringallto")|cmd.getName().equalsIgnoreCase("bah")){
-			if (!(args.length > 0)) {s.sendMessage(ChatColor.RED + "Needs a Player to teleport to!"); return true;}
-			Player Target = Bukkit.getServer().getPlayer(args[0]);
-			if (Target != null) {
-			Location tLoc = Target.getLocation();
-					for(Player p: Bukkit.getServer().getOnlinePlayers()){
-						if(p!=Target){p.teleport(tLoc);
-						p.sendMessage(ChatColor.GOLD+"Teleported everyone to "+Target.getName());}
+			if (s.hasPermission("bah.command.bringallto")|s.hasPermission("bah.command.*")) {
+				
+				if (!(args.length > 0)) {s.sendMessage(ChatColor.RED + "Needs a Player to teleport to!"); return true;}
+				Player Target = Bukkit.getServer().getPlayer(args[0]);
+				
+				if (Target != null) {
+					Location tLoc = Target.getLocation();
+					
+						for(Player p: Bukkit.getServer().getOnlinePlayers()){
+							if(!(p.hasPermission("bah.teleport.exempt"))){
+								p.teleport(tLoc);
+								p.sendMessage(ChatColor.GOLD+"Teleported everyone to "+Target.getName());}
+						}
+						
+						Target.sendMessage(ChatColor.GOLD + "Teleported everyone to you!");
+						s.sendMessage(ChatColor.GOLD + "Teleported everyone to "+Target.getName()+"!");
+						getLogger().info(s.getName() + " teleported all to " + Target.getName());
+						return true;
+						
+					} else {
+						
+						s.sendMessage(ChatColor.RED + args[0] + " was not found");
+						return true;
+						
 					}
-					Target.sendMessage(ChatColor.GOLD + "Teleported everyone to you!");
-					s.sendMessage(ChatColor.GOLD + "Teleported everyone to "+Target.getName()+"!");
-					getLogger().info(s.getName() + " teleported all to " + Target.getName());
+			} else {
+				s.sendMessage(ChatColor.RED + "You don't have permission!");
 					return true;
-				} else {
-					s.sendMessage(ChatColor.RED + args[0] + " was not found");
-					return true;
-					}
+			}
 		}
 		return true;
 	}
